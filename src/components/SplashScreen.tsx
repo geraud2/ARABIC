f4c928ca746a96328431955c2ba7d95db96c2a17
geraud2 <geraudhessou723@gmail.com>
2025-10-17 16:50:32 +0100

    first commit

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -27,7 +27,7 @@ const SplashScreen = () => {
         >
           {/* Logo Fisabil avec bord arrondi */}
           <img 
-            src="/fisa.jpeg" 
+            src="fisa.jpeg" 
             alt="Fisabil"
             className="w-40 h-40 mx-auto mb-4 object-cover rounded-full border-4 border-[#53B16F] shadow-lg"
           />
